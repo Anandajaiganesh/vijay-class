97b4a2aff9b0f76446ac70afd04f0c68a86cc51b
Anandajaiganesh <anandajaiganesh98@gmail.com>
2025-07-17 11:41:57 +0530

    1st push

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+//git commands
+
+//git init- initialize git empty repositary
+
+
+//1st time use
+
+//git config --global user.email "abc@gmail.com"
+//git config --global user.name "github username"
+
+//push
+//git add .
+
+
